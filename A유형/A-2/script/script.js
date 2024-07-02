9f520b1d83a0aba8148171fed27864fcc0e75019
무당선생 <yssmi@naver.com>
2024-07-02 14:49:45 +0900

    A-2 유형 완성, 디자인 전

---
+++ A유형/A-2/script/script.js
@@ -0,0 +1,35 @@
+// GNB
+$('.gnb > li').on('mouseenter', function () {
+  $('.nav_bg').stop().slideDown();
+  $('.submenu').stop().fadeIn();
+});
+$('.gnb > li').on('mouseleave', function () {
+  $('.nav_bg').stop().slideUp();
+  $('.submenu').stop().fadeOut();
+});
+
+// 이미지 슬라이드
+setInterval(function () {
+  $('.slide_wrap').animate(
+    {
+      'margin-left': '-100%',
+    },
+    1000,
+    function () {
+      $('.slide_item:first').appendTo('.slide_wrap');
+      $('.slide_wrap').css('margin-left', 0);
+    }
+  );
+}, 3000);
+
+// 팝업
+$('.open_popup').on('click', function (e) {
+  e.preventDefault();
+  $('.dim').fadeIn();
+  $('.popup').fadeIn();
+});
+
+$('.btn_close').on('click', function () {
+  $('.dim').fadeOut();
+  $('.popup').fadeOut();
+});
