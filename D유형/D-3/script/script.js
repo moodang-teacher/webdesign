57952a0b001d4cb1a1a0989cc180c2eadae2afa2
moodang <yssmi@naver.com>
2024-07-10 09:42:11 +0900

    D-3 완성

--- D유형/D-3/script/script.js
+++ D유형/D-3/script/script.js
@@ -3,11 +3,25 @@ $('.gnb > li').on('mouseenter', function () {
 	$('.submenu, .gnb_bg').stop().show();
 	$('.submenu').css('display', 'flex');
 });
-$('.gnb_bg').on('mouseleave', function () {
+$('.gnb > li').on('mouseleave', function () {
 	$('.submenu, .gnb_bg').stop().hide();
 });
 
 // 이미지 슬라이드 : fade In/Out
+$('.slide_item:gt(0)').hide();
+setInterval(function () {
+	$('.slide_item:first').fadeOut().next().fadeIn();
+	$('.slide_item:first').appendTo('.slide_wrap');
+}, 3000);
+
+// tab
+$('.tab_menu > a').on('click', function (e) {
+	e.preventDefault();
+	let tabIdx = $(this).index();
+
+	$('.tab_con_item').hide().eq(tabIdx).show();
+	$('.tab_menu > a').removeClass('on').eq(tabIdx).addClass('on');
+});
 
 // popup
 $('.open_popup').on('click', function (e) {
