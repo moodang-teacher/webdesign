9e058ac99e458d5947d5b80bb10c054eb2209528
yssmi <yssmi@naver.com>
2024-08-05 18:55:54 +0900

    D-3완성

--- D-type/D-3/script/script.js
+++ D-type/D-3/script/script.js
@@ -1,41 +1,65 @@
 // GNB
-// $('.gnb > li').on('mouseenter', function () {
-// 	$('.submenu, .gnb_bg').stop().show();
-// 	$('.submenu').css('display', 'flex');
-// });
-// $('.gnb > li').on('mouseleave', function () {
-// 	$('.submenu, .gnb_bg').stop().hide();
-// });
-
+// $('.gnb > li').on('mouseenter', function () {})
 $('.gnb > li').on('mouseenter', function () {
-	$('.submenu, .gnb_bg').stop().slideDown();
-});
-$('.gnb > li').on('mouseleave', function () {
-	$('.submenu, .gnb_bg').stop().slideUp();
+  $('.submenu, .gnb_bg').stop().fadeIn();
 });
 
-// 이미지 슬라이드 : fade In/Out
+$('.gnb > li').on('mouseleave', function () {
+  $('.submenu, .gnb_bg').stop().fadeOut();
+});
+
+
+// 페이드 슬라이더
+
+// 겹쳐진 slide_item중에서 첫번째를 제외하고 다 숨기자.
+console.log( $('.slide_item') );
 $('.slide_item:gt(0)').hide();
+
+// 3초에 한 번씩 실행
+// setInterval(function () {}, 3000)
 setInterval(function () {
-	$('.slide_item:first').fadeOut().next().fadeIn();
-	$('.slide_item:first').appendTo('.slide_wrap');
+
+  // 첫번째 슬라이드가 페이드 아웃
+  $('.slide_item:first').fadeOut();
+
+  // 첫번째 슬라이드의 바로 다음 동생을 페이드 인
+  $('.slide_item:first').next().fadeIn();
+
+  // 첫번째 슬라이드를 막내로 만들기
+  $('.slide_item:first').appendTo('.slide_wrap');
+
 }, 3000);
 
-// tab
-$('.tab_menu > a').on('click', function (e) {
-	e.preventDefault();
-	let tabIdx = $(this).index();
-
-	$('.tab_con_item').hide().eq(tabIdx).show();
-	$('.tab_menu > a').removeClass('on').eq(tabIdx).addClass('on');
-});
-
-// popup
-$('.open_popup').on('click', function (e) {
-	e.preventDefault();
-	$('.popup').fadeIn();
+// 팝업 띄우기
+// $('.open_popup').on('click', function () {})
+$('.open_popup').on('click', function () {
+  $('.popup').fadeIn();
 });
 
 $('.btn_close').on('click', function () {
-	$('.popup').fadeOut();
+  $('.popup').fadeOut();
 });
+
+// 탭메뉴
+
+// 갤러리 영역을 숨기고 시작 (첫번째를 남기고 나머지를 숨기기)
+$('.tab_con_item:gt(0)').hide();
+
+// 탭메뉴를 클릭했을 때
+// $('.tab_menu a').on('click', function () {})
+$('.tab_menu a').on('click', function (e) {
+  // a가 가진 기본동작 막기
+  e.preventDefault();
+
+  // 클릭한 메뉴의 인덱스를 구해서 변수 tabIdx에 담기
+  const tabIdx = $(this).index();
+  console.log(tabIdx);
+
+  // 인덱스가 같은 tab_con_item를 보이게 (일단 다 숨기고)
+  $('.tab_con_item').hide();
+  $('.tab_con_item').eq(tabIdx).show();
+
+  // 탭메뉴 활성화 표시 (일단 on클래스를 다 없애고)
+  $('.tab_menu a').removeClass('on');
+  $('.tab_menu a').eq(tabIdx).addClass('on');
+})
