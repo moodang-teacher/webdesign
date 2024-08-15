bdf638cc33fa45a98ae0edaf99423c4ced20ce3f
moodang <yssmi@naver.com>
2024-08-15 16:40:46 +0900

    E-3 완성

--- E-type/E-3/script/script.js
+++ E-type/E-3/script/script.js
@@ -1,31 +1,39 @@
+// 상하 슬라이더
+// setInterval(function(){}, 3000)
+setInterval(function(){
+
+  // $('.slide_wrap').animate({}, 1000, function(){})
+  $('.slide_wrap').animate({
+
+    'margin-top': '-800px'
+
+  }, 1000, function () {
+
+    $('.slide_item:first').appendTo('.slide_wrap');
+    $('.slide_wrap').css('margin-top', 0);
+  })
+
+}, 3000);
+
 // GNB
+// $('.gnb > li').on('mouseenter', function () {})
 $('.gnb > li').on('mouseenter', function () {
-	$(this).find('.submenu').show();
-});
+  $(this).find('.submenu').stop().fadeIn();
+})
 $('.gnb > li').on('mouseleave', function () {
-	$(this).find('.submenu').hide();
-});
+  $(this).find('.submenu').stop().fadeOut();
+})
+
 
 // popup
+// $('.open_popup').on('click', function () {})
 $('.open_popup').on('click', function (e) {
-	e.preventDefault();
-	$('.popup').fadeIn();
+  e.preventDefault();
+
+  $('.popup').fadeIn();
 });
 
 $('.btn_close').on('click', function () {
-	$('.popup').fadeOut();
+  $('.popup').fadeOut();
 });
 
-// slide
-setInterval(function () {
-	$('.slide_wrap').animate(
-		{
-			'margin-top': '-700px',
-		},
-		700,
-		function () {
-			$('.slide_item:first').appendTo('.slide_wrap');
-			$('.slide_wrap').css('margin-top', 0);
-		}
-	);
-}, 3000);
