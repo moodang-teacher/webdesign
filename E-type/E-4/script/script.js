c1071f894104bad1327a2fb2a60e12c41a9842eb
moodang <yssmi@naver.com>
2024-11-03 22:15:53 +0900

    11/3 진짜 최종

--- E-type/E-4/script/script.js
+++ E-type/E-4/script/script.js
@@ -1,31 +1,40 @@
+// 좌우 슬라이더
+
+// setInterval(function() {}, 3000)
+setInterval(function() {
+
+  // $('.slide_wrap').animate({}, 1000, function() {})
+  $('.slide_wrap').animate({
+  
+    'margin-left': '-100%'
+  
+  }, 1000, function() {
+  
+    $('.slide_wrap').css('margin-left', 0);
+    $('.slide_item:first').appendTo('.slide_wrap');
+  
+  });
+
+}, 3000)
+
+
 // GNB
-$('.gnb > li').on('mouseenter', function () {
-  $(this).find('.submenu').fadeIn();
-});
-$('.gnb > li').on('mouseleave', function () {
-  $(this).find('.submenu').fadeOut();
-});
+// $('.gnb > li').on('mouseenter', function() {})
+$('.gnb > li').on('mouseenter', function() {
+  $(this).find('.submenu').stop().fadeIn();
+})
 
-// slide
-setInterval(function () {
-  $('.slide_wrap').animate(
-    {
-      'margin-left': '-100%',
-    },
-    700,
-    function () {
-      $('.slide_item:first').appendTo('.slide_wrap');
-      $('.slide_wrap').css('margin-left', 0);
-    }
-  );
-}, 3000);
+$('.gnb > li').on('mouseleave', function() {
+  $(this).find('.submenu').stop().fadeOut();
+})
 
-// popup
-$('.open_popup').on('click', function () {
-  $('.dim').fadeIn();
-  $('.popup').fadeIn();
-});
+// popup 
+$('.open_popup').on('click', function(e) {
+  e.preventDefault();
 
-$('.btn_close').on('click', function () {
+  $('.dim, .popup').fadeIn();
+})
+
+$('.btn_close').on('click', function() {
   $('.dim, .popup').fadeOut();
-});
+})
