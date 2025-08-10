01b59054db7560a864afd386d73ad325c630d852
moodang <yssmi@naver.com>
2025-08-10 22:12:34 +0900

    D-2 새로운 코딩2

--- D-type/D-2/script/script.js
+++ D-type/D-2/script/script.js
@@ -1,43 +1,34 @@
 // 좌우 슬라이더
-// setInterval(function() {}, 3000);
-setInterval(function() {
-
-  // $('.slide_wrap').animate({옵션}, 시간, 종료후 동작)
-  // $('.slide_wrap').animate({}, 1000, function() {})
+// 3초에 한 번씩 실행
+// setInterval(동작, 시간)
+// setInterval(function () {}, 3000);
+setInterval(function () {
+  // $('.slide_wrap').animate({옵션}, 시간, 이후에 할 일)
   $('.slide_wrap').animate({
-
     'margin-left': '-100%'
-
-  }, 1000, function() {
-
-    // 위치를 재조정
-    $('.slide_wrap').css('margin-left', 0);
-
-    // 첫번째 슬라이드를 맨 마지막으로 보내기
+  }, 1000, function () {
     $('.slide_item:first').appendTo('.slide_wrap');
-    
+    $('.slide_wrap').css('margin-left', '0');
   })
-
 }, 3000);
 
 
 // GNB
-// $('.gnb > li').on('mouseenter', function() {})
-$('.gnb > li').on('mouseenter', function() {
+// $('.gnb > li').on('mouseenter', function () {})
+$('.gnb > li').on('mouseenter', function () {
   $(this).find('.submenu').stop().slideDown();
 })
-$('.gnb > li').on('mouseleave', function() {
+$('.gnb > li').on('mouseleave', function () {
   $(this).find('.submenu').stop().slideUp();
 })
 
-// popup
-// $('.open_popup').on('click', function() {})
-$('.open_popup').on('click', function(e) {
-  // a의 기본동작(링크이동)을 막기
-  e.preventDefault();
-  $('.dim, .popup').fadeIn();
+
+// 팝업
+// $('.open_popup').on('click', function () {})
+$('.open_popup').on('click', function () {
+  $('.popup, .dim').fadeIn();
 })
 
-$('.btn_close').on('click', function() {
-  $('.dim, .popup').fadeOut();
+$('.btn_close').on('click', function () {
+  $('.popup, .dim').fadeOut();
 })
