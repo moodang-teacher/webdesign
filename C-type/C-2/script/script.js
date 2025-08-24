ce60a53387812b5d8f0195172057520da9ca46c5
moodang <yssmi@naver.com>
2025-08-24 20:58:48 +0900

    C-2 부여 연꽃축제 업데이트

--- C-type/C-2/script/script.js
+++ C-type/C-2/script/script.js
@@ -1,35 +1,41 @@
-// fade slider
-// 현재 겹쳐져있는 상태, 맨 처음 슬라이드 제외한 나머지를 숨기고 시작
-// = 인덱스가 0보다 큰 슬라이드를 숨기자 --> 대상:gt(0).hide()
+// 페이드 슬라이더
+console.log( $('.slide_item') );
 
-$(".slide_item:gt(0)").hide();
+// 현재는 겹쳐있는 상태, 첫번째 슬라이드를 제외한 나머지를 숨기고 시작!
+$('.slide_item:gt(0)').hide();
 
 // 3초에 한 번씩 실행
-// setInterval(function(){}, 3000)
+// setInterval(동작, 시간)
+// setInterval(function () {}, 3000);
 setInterval(function () {
-  // 첫번째 슬라이드가 페이드 아웃
-  $(".slide_item:first").fadeOut();
+  // 첫번째 슬라이드가 페이드 아웃(사라짐)
+  $('.slide_item:first').fadeOut();
 
-  // 그 첫번째 슬라이드의 바로 다음 동생은 페이드 인
-  $(".slide_item:first").next().fadeIn();
+  // 두번째 슬라이드가 페이드 인(보여짐) --> 첫번째 슬라이드의 바로 다음 동생
+  $('.slide_item:first').next().fadeIn();
 
-  // 첫번째 슬라이드를 부모 기준, 막내로 만들자
-  $(".slide_item:first").appendTo(".slide_wrap");
+  // 첫번째 슬라이드가 막내가 되어야 함
+  $('.slide_item:first').appendTo('.slide_wrap');
 }, 3000);
-
-// 팝업
-$(".open_popup").on("click", function () {
-  $(".dim, .popup").fadeIn();
-});
-$(".btn_close").on("click", function () {
-  $(".dim, .popup").fadeOut();
-});
 
 // GNB
 // $('.gnb > li').on('mouseenter', function () {})
-$(".gnb > li").on("mouseenter", function () {
-  $(this).find(".submenu").stop().slideDown();
+$('.gnb > li').on('mouseenter', function () {
+  $(this).find('.submenu').stop().slideDown();
+})
+
+$('.gnb > li').on('mouseleave', function () {
+  $(this).find('.submenu').stop().slideUp();
+})
+
+// console.log( $('.gnb > li') );
+
+// 팝업
+// $('.open_popup').on('click', function () {})
+$('.open_popup').on('click', function () {
+  $('.popup, .dim').fadeIn();
 });
-$(".gnb > li").on("mouseleave", function () {
-  $(this).find(".submenu").stop().slideUp();
+
+$('.btn_close').on('click', function () {
+  $('.popup, .dim').fadeOut();
 });
