9c1f9ca5c70c9f8d8e406f7285ea6df748e8b3a7
moodang <yssmi@naver.com>
2025-08-24 20:54:38 +0900

    E-4 철길마을 업데이트

--- E-type/E-4/script/script.js
+++ E-type/E-4/script/script.js
@@ -1,40 +1,38 @@
 // 좌우 슬라이더
+// 3초에 한 번씩 실행
+// setInterval(function () {}, 3000)
+setInterval(function () {
 
-// setInterval(function() {}, 3000)
-setInterval(function() {
-
-  // $('.slide_wrap').animate({}, 1000, function() {})
+  // $('.slide_wrap').animate({}, 1000, function () {})
   $('.slide_wrap').animate({
-  
     'margin-left': '-100%'
-  
-  }, 1000, function() {
-  
-    $('.slide_wrap').css('margin-left', 0);
+  }, 1000, function () {
     $('.slide_item:first').appendTo('.slide_wrap');
-  
-  });
+    $('.slide_wrap').css('margin-left', '0');
+  })
 
 }, 3000)
 
 
 // GNB
-// $('.gnb > li').on('mouseenter', function() {})
-$('.gnb > li').on('mouseenter', function() {
+// $('.gnb > li').on('mouseenter', function () {})
+$('.gnb > li').on('mouseenter', function () {
+  // $('.submenu').fadeIn();
   $(this).find('.submenu').stop().fadeIn();
 })
 
-$('.gnb > li').on('mouseleave', function() {
+$('.gnb > li').on('mouseleave', function () {
+  // $('.submenu').fadeIn();
   $(this).find('.submenu').stop().fadeOut();
 })
 
-// popup 
-$('.open_popup').on('click', function(e) {
-  e.preventDefault();
 
-  $('.dim, .popup').fadeIn();
+// 팝업
+// $('.open_popup').on('click', function () {})
+$('.open_popup').on('click', function () {
+  $('.popup, .dim').fadeIn();
 })
 
-$('.btn_close').on('click', function() {
-  $('.dim, .popup').fadeOut();
+$('.btn_close').on('click', function () {
+  $('.popup, .dim').fadeOut();
 })
