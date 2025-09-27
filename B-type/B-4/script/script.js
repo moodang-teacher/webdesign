f430b1344111a7dace9c9a199201497a0b57219a
moodang <yssmi@naver.com>
2025-09-27 11:41:41 +0900

    B-3, B-4 수정 및 추가

--- B-type/B-4/script/script.js
+++ B-type/B-4/script/script.js
@@ -1,37 +1,36 @@
-// 좌우 슬라이더
-
-// setInterval(function () {}, 3000)
+// 좌우 슬라이드
+// setInterval(동작, 시간)
+// setInterval(function () {}, 3000);
 setInterval(function () {
   // $('.slide_wrap').animate({}, 1000, function () {})
-  $(".slide_wrap").animate(
-    {
-      "margin-left": "-100%",
-    },
-    1000,
-    function () {
-      $(".slide_item:first").appendTo(".slide_wrap");
-      $(".slide_wrap").css("margin-left", 0);
-    }
-  );
+  $('.slide_wrap').animate({
+    'margin-left': '-1200px'
+  }, 1000, function () {
+    $('.slide_item:first').appendTo('.slide_wrap');
+    $('.slide_wrap').css('margin-left', '0');
+  })
 }, 3000);
 
 // GNB
 // $('.gnb > li').on('mouseenter', function () {})
-$(".gnb > li").on("mouseenter", function () {
-  $(".submenu, .gnb_bg").stop().slideDown();
-});
+$('.gnb > li').on('mouseenter', function () {
+  $('.submenu, .gnb_bg').stop().slideDown();
+})
 
-$(".gnb > li").on("mouseleave", function () {
-  $(".submenu, .gnb_bg").stop().slideUp();
-});
+$('.gnb > li').on('mouseleave', function () {
+  $('.submenu, .gnb_bg').stop().slideUp();
+})
 
-// popup
-$(".open_popup").on("click", function (e) {
+
+// 팝업
+// $('.open_popup').on('click', function () {})
+$('.open_popup').on('click', function (e) {
+  // a의 기본동작을 막기
   e.preventDefault();
 
-  $(".dim, .popup").fadeIn();
-});
+  $('.dim, .popup').fadeIn();
+})
 
-$(".btn_close").on("click", function () {
-  $(".dim, .popup").fadeOut();
-});
+$('.btn_close').on('click', function () {
+  $('.dim, .popup').fadeOut();
+})
