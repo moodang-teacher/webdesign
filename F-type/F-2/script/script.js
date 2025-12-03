edee644af891f95e4f6cb4f6e8b9053deacecf11
moodang <yssmi@naver.com>
2025-12-03 16:48:33 +0900

    F-2 업데이트

--- F-type/F-2/script/script.js
+++ F-type/F-2/script/script.js
@@ -1,40 +1,32 @@
-// 페이드 슬라이더
+// 페이드 슬라이드
+console.log($('.slide_item:gt(0)'));
 
-// setInterval(동작, 시간)
-// setInterval(function () {}, 3000);
-
-// 1. 첫번째 슬라이드를 제외한 나머지를 숨긴다.
+// 첫번째 .slide_item을 제외한 나머지를 숨기고 시작
 $('.slide_item:gt(0)').hide();
 
-// console.log( $('.slide_item') );
-
+// 3초에 한 번씩 실행
 setInterval(function () {
-
-  // 2. 첫번째 슬라이드를 fadeOut, 그 다음 슬라이드를 fadeIn
-  // $('.slide_item:first').fadeOut();
-  // $('.slide_item:first').next().fadeIn();
-
-  $('.slide_item:first').fadeOut().next().fadeIn();
-
-  // 3. 첫번째 슬라이드를 막내로 만들기
+  $('.slide_item:first').fadeOut();
+  $('.slide_item:first').next().fadeIn();
   $('.slide_item:first').appendTo('.slide_wrap');
 }, 3000);
 
 
 // GNB
-$('.gnb > li').on('mouseenter', function() {
+$('.gnb > li').on('mouseenter', function () {
   $(this).find('.submenu').stop().slideDown();
 })
-$('.gnb > li').on('mouseleave', function() {
+
+$('.gnb > li').on('mouseleave', function () {
   $(this).find('.submenu').stop().slideUp();
 })
 
-// popup
-$('.open_popup').on('click', function () {
+// POPUP
+$('.open_popup').on('click', function (e) {
+  e.preventDefault();
   $('.popup, .dim').fadeIn();
-});
+})
 
 $('.btn_close').on('click', function () {
   $('.popup, .dim').fadeOut();
-});
-
+})
