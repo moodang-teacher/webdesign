1f6065b0529a887380417291fd6ef2a49a18cabe
moodang <yssmi@naver.com>
2025-12-03 16:47:55 +0900

    F-1 업데이트

--- F-type/F-1/script/script.js
+++ F-type/F-1/script/script.js
@@ -1,61 +1,61 @@
-// 페이드 슬라이더
+// fade in/out 슬라이드
 
-// console.log( $('.slide_item') );
+// console.log($('.slide_item'));
 
-// 첫번째 슬라이드만 보여지고 나머지는 숨겨라
+// 첫번째 요소를 제외한 나머지를 숨긴다. 
+// --> 0번째 인덱스보다 큰 인덱스를 가진 요소를 숨겨라
 $('.slide_item:gt(0)').hide();
 
 // 3초에 한 번씩 실행
-// setInterval(function() {}, 3000)
+// setInterval(동작, 시간);
 setInterval(function () {
-	// 첫번째 슬라이드는 사라지고
-	$('.slide_item:first').fadeOut();
-
-	// 첫번째 슬라이드의 바로 다음 동생이 나타나고
-	$('.slide_item:first').next().fadeIn();
-
-	// 첫번째 슬라이드를 맨 마지막으로 위치 이동
-	$('.slide_item:first').appendTo('.slide_wrap');
+  $('.slide_item:first').fadeOut();
+  $('.slide_item:first').next().fadeIn();
+  $('.slide_item:first').appendTo('.slide_wrap');
 }, 3000);
 
+
 // GNB
-// $('.gnb > li').on('mouseenter', function() {})
 $('.gnb > li').on('mouseenter', function () {
-	$(this).find('.submenu').stop().slideDown();
+  $(this).find('.submenu').stop().slideDown();
 });
+
 $('.gnb > li').on('mouseleave', function () {
-	$(this).find('.submenu').stop().slideUp();
+  $(this).find('.submenu').stop().slideUp();
 });
 
-// 팝업
-$('.open_popup').on('click', function (e) {
-	e.preventDefault(); // a의 기본동작(링크 이동) 막기
-	$('.popup').fadeIn();
-});
-$('.btn_close').on('click', function () {
-	$('.popup').fadeOut();
-});
+// TAB
+console.log($('.tab_con_item'));
 
-// 탭메뉴
-// 첫번째를 제외한 나머지 .tab_con_item 숨기기
+// 첫번째를 제외한 나머지를 숨기고 시작 --> 인덱스
 $('.tab_con_item:gt(0)').hide();
 
-// 탭메뉴를 클릭했을 때
+// 사용자가 클릭한 메뉴의 인덱스를 구해서, 
 $('.tab_menu a').on('click', function (e) {
-	// a의 기본동작 막기
-	e.preventDefault();
+  e.preventDefault();
+  const tabIndex = $(this).index();
+  console.log(tabIndex);
 
-	// 클릭한 요소의 인덱스 구하기
-	const tabIndex = $(this).index();
-	console.log(tabIndex);
+  // 일단, 모두 숨기고
+  $('.tab_con_item').hide();
 
-	// 일단 다 숨기고
-	$('.tab_con_item').hide();
+  // 그 인덱스에 해당하는 .tab_con_item을 보여주자
+  $('.tab_con_item').eq(tabIndex).show();
 
-	// 인덱스에 해당하는 .tab_con_item 보이게
-	$('.tab_con_item').eq(tabIndex).show();
+  // 메뉴의 활성화 표시
+  // 1. 일단 모두 제거하고: removeClass('on')
+  $('.tab_menu a').removeClass('on');
 
-	// 탭메뉴에 있는 on클래스를 다 지우고 클릭한 인덱스에 해당하는 메뉴에만 on클래스 부여
-	$('.tab_menu a').removeClass('on');
-	$('.tab_menu a').eq(tabIndex).addClass('on');
+  // 2. 선택한 인덱스에 해당하는 메뉴에 클래스 부여: addClass('on')
+  $('.tab_menu a').eq(tabIndex).addClass('on');
 });
+
+// POPUP
+$('.open_popup').on('click', function (e) {
+  e.preventDefault();
+  $('.popup').fadeIn();
+})
+
+$('.btn_close').on('click', function () {
+  $('.popup').fadeOut();
+})
